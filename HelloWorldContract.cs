8181862733cd76c401212ebdfd613a131e55e912
Harry Pierson <harrypierson@hotmail.com>
2023-02-02 14:45:25 -0800

    deploy/update methods

--- HelloWorldContract.cs
+++ HelloWorldContract.cs
@@ -15,7 +15,7 @@ namespace DevHawk.SampleContracts
     public class HelloWorldContract : SmartContract
     {
         const byte Prefix_SampleValue = 0x00;
-        // const byte Prefix_ContractOwner = 0xFF;
+        const byte Prefix_ContractOwner = 0xFF;
 
         [Safe]
         public static ByteString Get()
@@ -39,28 +39,28 @@ namespace DevHawk.SampleContracts
             Storage.Delete(context, key);
         }
 
-        // [DisplayName("_deploy")]
-        // public static void Deploy(object _, bool update)
-        // {
-        //     if (update) return;
+        [DisplayName("_deploy")]
+        public static void Deploy(object _, bool update)
+        {
+            if (update) return;
 
-        //     var tx = (Transaction)Runtime.ScriptContainer;
-        //     var key = new byte[] { Prefix_ContractOwner };
-        //     Storage.Put(Storage.CurrentContext, key, tx.Sender);
-        // }
+            var tx = (Transaction)Runtime.ScriptContainer;
+            var key = new byte[] { Prefix_ContractOwner };
+            Storage.Put(Storage.CurrentContext, key, tx.Sender);
+        }
 
-        // public static void Update(ByteString nefFile, string manifest)
-        // {
-        //     var key = new byte[] { Prefix_ContractOwner };
-        //     var contractOwner = (UInt160)Storage.Get(Storage.CurrentContext, key);
-        //     var tx = (Transaction)Runtime.ScriptContainer;
+        public static void Update(ByteString nefFile, string manifest)
+        {
+            var key = new byte[] { Prefix_ContractOwner };
+            var contractOwner = (UInt160)Storage.Get(Storage.CurrentContext, key);
+            var tx = (Transaction)Runtime.ScriptContainer;
 
-        //     if (!contractOwner.Equals(tx.Sender))
-        //     {
-        //         throw new Exception("Only the contract owner can update the contract");
-        //     }
+            if (!contractOwner.Equals(tx.Sender))
+            {
+                throw new Exception("Only the contract owner can update the contract");
+            }
 
-        //     ContractManagement.Update(nefFile, manifest, null);
-        // }
+            ContractManagement.Update(nefFile, manifest, null);
+        }
     }
 }
