a484ffb6e58b87e2227fb3d2b9f860d28d6eab74
Harry Pierson <harrypierson@hotmail.com>
2023-02-02 14:42:12 -0800

    initial commit

---
+++ HelloWorldContract.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Services;
+
+namespace DevHawk.SampleContracts
+{
+    public class HelloWorldContract : SmartContract
+    {
+        const byte Prefix_ContractOwner = 0xFF;
+
+        [Safe]
+        public static string SayHello(string name)
+        {
+            return $"Hello, {name}!";
+        }
+
+        [DisplayName("_deploy")]
+        public void Deploy(object data, bool update)
+        {
+            if (update) return;
+
+            var tx = (Transaction)Runtime.ScriptContainer;
+            var key = new byte[] { Prefix_ContractOwner };
+            Storage.Put(Storage.CurrentContext, key, tx.Sender);
+        }
+    }
+}
